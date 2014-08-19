03968178a89e73d86fc1f7fa015402f11f2cc273
sachour <sachour@mit.edu>
2014-08-19 14:49:42 -0400

    Added cover page description

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var navigateTo = function(handle){
 }
 
 $(document).ready(function(){
-	$("#office").html("#G730, Ray and Maria Stata Center<br>MIT, Cambridge, MA");
+	$("#office").html("#G730, Ray and Maria Stata Center<br>MIT, Cambridge, MA, 02139");
 	$("#email").attr("href","mailto:sachour@csail.mit.edu")
 		.html("sachour&#64;csail.mit.edu");
 	$("#github").attr("href","http://github.com/sarachour")
