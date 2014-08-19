d5bc6bfbc4c17cbd34db5babf75227b88043e935
sachour <sachour@mit.edu>
2014-08-19 10:26:46 -0400

    added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$("#office").html("#G730, Ray and Maria Stata Center<br>MIT, Cambridge, MA");
+	$("#email").attr("href","mailto:sachour@csail.mit.edu")
+		.html("sachour&#64;csail.mit.edu");
+	$("#github").attr("href","http://github.com/sarachour")
+		.html("github.com/sarachour");
+})
