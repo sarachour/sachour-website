225d7a9a028a21aceb49ce08a8070eafd294dae4
sachour <sachour@mit.edu>
2015-08-25 16:47:00 -0400

    overhaul

---
+++ js/data.js
@@ -0,0 +1,210 @@
+var ABOUTME = `I am a second year PhD Student in 
+            <a href="http://www.eecs.mit.edu/">Electrical Engineering and 
+            Computer Science</a> at MIT and am part of the Programming and Computing group at 
+            <a href="http://www.csail.mit.edu/">CSAIL</a>. Currently, I am advised by
+            <a href="http://people.csail.mit.edu/rinard/">Martin Rinard</a>. I completed my 
+            undegraduate degree at UCLA and attained a dual degree in <a href="">Computer Science</a> and 
+            <a href="">Computational Biology</a>. I am interested in approximate computation, security and 
+            programming models.`
+
+var NEWS = [
+   {
+      title:"Awarded NSF GRFP Fellowship",
+      desc:"I was awarded a graduate research fellowship from the National Science Foundation. I was one of 88 awardees in the Field of Computer Science.",
+      date:"April 30,2015"
+   }
+]
+
+var PUBS = [
+   {
+      title:"Approximate Computation With Outlier Detection in Topaz",
+      kind:"conf",
+      authors:["Sara Achour","Martin Rinard"],
+      conf:"OOPSLA 2015.",
+      date:"October 2015",
+      url:"",
+      artifact:""
+   },
+   {
+      title:"An Analysis of Patch Plausibility and Correctness for Generate-And-Validate Patch Generation Systems",
+      kind:"conf",
+      authors:["Zichao Qi","Fan Long","Sara Achour","Martin Rinard"],
+      conf:"ISSTA 2015",
+      date:"July 2015",
+      url:"",
+      artifact:""
+   },
+   {
+      title:"Chisel: Reliability- and Accuracy-Aware Optimization of Approximate Computational Kernels",
+      kind:"conf",
+      authors:["Sasa Misailovic","Michael Carbin","Sara Achour","Zichao Qi","Martin Rinard"],
+      conf:"OOPSLA 2014, Best Paper",
+      date:"October 2014",
+      url:""
+   },
+   {
+      title:"Energy Efficient Computation in Topaz",
+      kind:"thesis",
+      authors:["Sara Achour","Martin Rinard"],
+      conf:"OOPSLA 2015.",
+      date:"January 2015",
+      url:""
+   },
+   {
+      title:"Automatic Program Repair with Condition Synthesis and Compound Mutations ",
+      kind:"tr",
+      authors:["Zichao Qi","Fan Long","Sara Achour","Martin Rinard"],
+      date:"February 2015",
+      url:"http://18.7.29.232/handle/1721.1/94520"
+
+   }
+]
+
+var RESEARCH = [
+   {
+      title:"Topaz",
+      icon:"images/topaz-icon.png",
+      type:"grad",
+      desc:`Topaz is a task-based language and runtime for developing programs to run on 
+         approximate computing platforms that may occasionally produce arbitrarily inaccurate results. 
+         The Topaz implementation maps tasks onto approximate machines and integrates results
+         into the main computation, deploying a novel outlier detection and reliable reexecution mechanism to 
+         prevent unacceptably inaccurate results from corrupting the overall computation.`,
+      startdate:"2014",
+      enddate:"2015",
+      show_summary:true
+   },
+   {
+      title:"Chisel",
+      icon:"images/chisel-icon.png",
+      type:"grad",
+      desc:`Chisel is a system for reliability- and accuracy-aware optimization of approximate computational kernels 
+            that run on approximate hardware platforms. Given a combined reliability and accuracy specification, 
+            Chisel automatically selects approximate kernel operations to synthesize an approximate computation 
+            that minimizes energy consumption while satisfying its reliability and accuracy specification.`,
+      startdate:"2013",
+      enddate:"2014"
+   },
+   {
+      title:"Magpie",
+      icon:"images/magpie-icon.png",
+      type:"grad",
+      desc:`Magpie is a code reuse detection tool that, given a Java project, scans online repositories for similar 
+            snippets of code. Magpie then communicates the results to the user through the code reuse summary and browser. This tool 
+            was implemented as an Eclipse plugin.`,
+      startdate:"Jan 2014",
+      enddate:"Mar 2014"
+   },
+   {
+      title:"Matlab to Habanero C Compiler",
+      icon:"images/habenero-icon.png",
+      type:"undergrad",
+      desc:`The Matlab to <a href="">Habanero C</a> compiler cross-compiles a subset of the Matlab language to Habanero C. The compiler has 
+            partial library function support and links generated applications with a parallelized math library comprised of
+            <a href="">LaPack</a>, <a href="">Blas</a> and handwritten parallel code. The cross compiler also recognizes and optimizes for cross-cutting
+            matrix operations. I conducted this research under <a href="">Jens Palsberg</a>'s mentorship as part of the <a>Center for
+            Domain Specific Computing (CDSC)</a> undergraduate 
+            research program.`,
+      startdate:"Jun 2012",
+      enddate:"Aug 2012"
+   },
+   {
+      title:"Thyroid Simulator",
+      icon:"images/bio-icon.png",
+      type:"undergrad",
+      desc:`I wrote a partial differential equation simulator for the pediatric thyroid hormone model. The simulator handled upwards of
+            eighty differential equations and supported time delayed state variables and discontinuous inputs. The thyroid
+            model itself is a very stiff system and is challenging to simulate. This project was part of my Computational Biology 
+            thesis.`,
+      startdate:"2009",
+      enddate:"2010"
+   }
+]
+
+var WORK = [
+   {
+      title:"Qualcomm Research Intern",
+      desc:`I interned in the San Diego corporate Research and Development office on project Neo for 
+      two consecutive summers. For my internship, I developed high throughput realtime neurological 
+      visualizations to facilitate in-browser monitoring of simulations. I was one of the six interns 
+      awarded the Roberto Padovani scholarship and returned to continue my work a second year.`,
+      startdate:"2012",
+      enddate:"2013"
+   },
+   {
+      title:"Salk Institute Research Intern",
+      desc:`I interned at the Razavi Newman Bioinformatics research laboratory at the Salk Institute under Dr. Gerard Manning for several summers. For my research,
+            I annotated the <em>Paramecium</em> kinome and used comparative genomics to draw conclusions about the evolution of certain kinase families in this species.`,
+      startdate:"2006",
+      enddate:"2008"
+   }
+]
+
+var SIDEPROJ = [
+   {
+      title:"8Tracks Virtuoso",
+      icon:"images/virtuoso-icon.png",
+      desc:`Virtuoso is a Chrome extension for power users of 8tracks. Features include exporting mixes and tracks to Spotify, 
+            mapping global hotkeys to common tasks, automatically scrobbling and synchronizing liked tracks with Last.FM and "now playing" 
+            popup notifications. Check it out <a href="https://chrome.google.com/webstore/detail/8tracks-virtuoso/ghihkgpgokfnlkldpphclkcdjidhdmda?hl=en">here</a>.`
+   },
+   {
+      title:"Debauchery",
+      icon:"images/debauchary-icon.png",
+      desc:`Debauchery is a collaborative drawing game. Development currently in progress and the mobile application will be released shortly.`
+   },
+   {
+      title:"Scramble",
+      icon:"images/scramble-icon.png",
+      desc:`Scramble is an in-browser game engine that enables users to collaboratively play retro games using a variety of 
+            consensus rules. Development currently in progress and the site will be deployed in the near future.`
+
+   }
+]
+
+var HOBBIES = [
+   {
+      title:"Art",
+      icon:"images/art-icon.png",
+      desc:`I enjoy sculpting, drawing and assembling papercraft models in my spare time.`
+   },
+   {
+      title:"Travel",
+      icon:"images/travel-icon.png",
+      desc:`I enjoy travelling - especially to other cities. One of the best parts about attending conferences 
+      is travelling to all of these locations you would have otherwise never thought to visit.`
+   },
+   {
+      title:"Food and Drink",
+      icon:"images/beer-icon.png",
+      desc:`I enjoy fancy food and drink - especially craft brews!`
+   }
+]
+var TRAVEL = [
+   {
+      name:"Accomplice",
+      place:"Manhattan,NY",
+      description:"An interactive play that revolves around helping the mafia where attendees run through New York city doing errands for incognito actors.",
+
+   },
+   {
+      name:"Sleep No More",
+      place:"Manhattan,NY",
+      description:"A Bioshock-esque retelling of MacBeth in 1950s america. Masked attendees wander through a hotel, following whatever interests them.",
+
+   },
+   {
+      name:"mmuseumm",
+      place:"Manhattan,NY",
+      description:"A tiny museum in a freight elevator in a random alleyway. Filled with a lot of quirky exhibits - super fun.",
+
+   },
+   {
+      name:"Prohibition Bakery",
+      place:"Manhattan,NY",
+      description:"cocktails and mixed drinks in alcoholic cupcake form.",
+
+   }
+
+
+]
--- js/main.js
+++ js/main.js
@@ -1,42 +1,12 @@
 
-var travel = [
-	{
-		id:0,
-		name:"Accomplice",
-		place:"Manhattan,NY",
-		description:"An interactive play that revolves around helping the mafia where attendees run through New York city doing errands for incognito actors.",
-
-	},
-	{
-		id:1,
-		name:"Sleep No More",
-		place:"Manhattan,NY",
-		description:"A Bioshock-esque retelling of MacBeth in 1950s america. Masked attendees wander through a hotel, following whatever interests them.",
-
-	},
-	{
-		id:2,
-		name:"mmuseumm",
-		place:"Manhattan,NY",
-		description:"A tiny museum in a freight elevator in a random alleyway. Filled with a lot of quirky exhibits - super fun.",
-
-	},
-	{
-		id:3,
-		name:"Prohibition Bakery",
-		place:"Manhattan,NY",
-		description:"cocktails and mixed drinks in alcoholic cupcake form.",
-
-	}
-
-
-]
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 
 var populateTravel = function(){
 	var parent = $("#travel");
 	for(var i=0; i < travel.length; i++){
 		var e = travel[i];
-		console.log(e);
 		var elem = $("<div/>").addClass("travel entry");
 		var title = $("<span/>").addClass("travel title").html(e.name);
 		var place = $("<span/>").addClass("travel place").html(e.place);
@@ -47,17 +17,132 @@ var populateTravel = function(){
 
 }
 
-var navigateTo = function(handle){
-	if($(".selected#"+handle, $("#navigation")).length == 1)
-		return;
-	$(".navigate.selected", $("#navigation")).removeClass("selected");
-	$("#"+handle, $("#navigation")).addClass("selected");
 
-	$(".page.selected", $(".pages")).removeClass("selected").hide();
-	$("#"+handle, $(".pages")).addClass("selected").fadeIn(300);
+
+var get_authstr = function(auths){
+	var author_str = "";
+	for(var j=0; j < auths.length; j++){
+		var auth = auths[j];
+
+		if(j < auths.length-2) 
+			var delim = ", "
+		else if(j == auths.length-2) 
+			var delim = " and "
+		else 
+			var delim = "";
+
+		if(auth == "Sara Achour"){
+			author_str += "<u>"+auth+"</u>"+delim
+		}
+		else{
+			author_str += auth+delim
+		}
+	}
+	return author_str;
 }
 
-$(document).ready(function(){
+var load_pubs = function(){
+	var cntr = $("#pub_cont");
+	var tmpl = $("#pub_tmpl");
+
+	
+	for(var i=0; i < PUBS.length; i++){
+		var vl = PUBS[i];
+		var el = tmpl.clone();
+		var date = new Date(vl.date);
+		$("#title",el).html(vl.title);
+		$("#authors",el).html(get_authstr(vl.authors));
+		if(vl.kind == "conf"){
+			$("#conf",el).html(vl.conf);
+		}
+		else if(vl.kind == "tr") {
+			$("#conf",el).html("Technical Report");
+		}
+		else if(vl.kind == "thesis") {
+			$("#conf",el).html("Thesis");
+		}
+		$("#date",el).html(monthNames[date.getMonth()] +" "+ date.getFullYear());
+		if(vl.url != "" && vl.url != undefined){
+			$("#link",el).attr("href",vl.url)
+		}
+		else{
+			$("#link",el).attr("href","")
+				.html("paper unavailable")
+				.css("pointer-events","none")
+				.css("color","grey")
+		}
+		if(vl.artifact != "" && vl.artifact != undefined){
+			$("#artifact",el).attr("href",vl.url)
+		}
+		else{
+			$("#artifact",el).attr("href","")
+				.html("no artifact")
+				.css("pointer-events","none")
+				.css("color","grey")
+		}
+		cntr.append(el)
+	}
+	tmpl.remove();
+
+}
+var load_news = function(){
+	var cntr = $("#news_cont");
+	var tmpl = $("#news_tmpl");
+
+	for(var i=0; i < NEWS.length; i++){
+		var vl = NEWS[i];
+		var el = tmpl.clone();
+		var date = new Date(vl.date);
+		$("#title",el).html(vl.title);
+		$("#desc",el).html(vl.desc);
+		$("#date",el).html(date.toDateString());
+		cntr.append(el)
+	}
+	tmpl.remove();
+}
+
+var load_research_summaries = function(){
+	var cntr = $("#research_summ_cont");
+	var tmpl = $("#research_summ_tmpl");
+
+	for(var i=0; i < RESEARCH.length; i++){
+		var vl = RESEARCH[i];
+		if(vl.show_summary == true){
+			var el = tmpl.clone();
+			var startdate = new Date(vl.startdate);
+			var enddate = new Date(vl.enddate);
+			$("#title",el).html(vl.title);
+			$("#desc",el).html(vl.desc);
+			$("#icon",el).attr("src",vl.icon);
+			$("#date",el).html(startdate.getFullYear()+"-"+enddate.getFullYear());
+			cntr.append(el)
+		}
+	}
+	tmpl.remove();
+}
+
+var load_research = function(){
+	var cntr = $("#research_cont");
+	var tmpl = $("#research_tmpl");
+
+	for(var i=0; i < RESEARCH.length; i++){
+		var vl = RESEARCH[i];
+		var el = tmpl.clone();
+		var startdate = new Date(vl.startdate);
+		var enddate = new Date(vl.enddate);
+		$("#title",el).html(vl.title);
+		$("#desc",el).html(vl.desc);
+		$("#icon",el).attr("src",vl.icon);
+		$("#date",el).html(startdate.getFullYear()+"-"+enddate.getFullYear());
+		cntr.append(el)
+	}
+	tmpl.remove();
+}
+
+var load_aboutme = function(){
+	$("#aboutme").html(ABOUTME);
+}
+var load_links = function(){
 	$("#resume").attr("href", "resources/resume.pdf").html("resume");
 	$("#office").html("#G730, Ray and Maria Stata Center<br>MIT, Cambridge, MA, 02139");
 	$("#email").attr("href","mailto:sachour@csail.mit.edu")
@@ -65,11 +150,84 @@ $(document).ready(function(){
 	$("#github").attr("href","http://github.com/sarachour")
 		.html("github");
 
+}
+var load_industry = function(){
+	var cntr = $("#industry_cont");
+	var tmpl = $("#industry_tmpl");
+
+	for(var i=0; i < WORK.length; i++){
+		var vl = WORK[i];
+		var el = tmpl.clone();
+		var startdate = new Date(vl.startdate);
+		var enddate = new Date(vl.enddate);
+		$("#title",el).html(vl.title);
+		$("#desc",el).html(vl.desc);
+		$("#date",el).html(startdate.getFullYear()+"-"+enddate.getFullYear());
+		cntr.append(el)
+	}
+	tmpl.remove();
+}
+
+var load_sideproj = function(){
+	var cntr = $("#side_cont");
+	var tmpl = $("#side_tmpl");
+
+	for(var i=0; i < SIDEPROJ.length; i++){
+		var vl = SIDEPROJ[i];
+		var el = tmpl.clone();
+		$("#title",el).html(vl.title);
+		$("#desc",el).html(vl.desc);
+		$("#icon",el).attr("src",vl.icon);
+		cntr.append(el)
+	}
+	tmpl.remove();
+}
+
+
+var load_personal = function(){
+	var cntr = $("#pers_cont");
+	var tmpl = $("#pers_tmpl");
+
+	for(var i=0; i < HOBBIES.length; i++){
+		var vl = HOBBIES[i];
+		var el = tmpl.clone();
+		$("#title",el).html(vl.title);
+		$("#desc",el).html(vl.desc);
+		$("#icon",el).attr("src",vl.icon);
+		cntr.append(el)
+	}
+	tmpl.remove();
+}
+var load_all = function(){
+	load_links();
+	load_aboutme();
+	load_news();
+	load_pubs();
+	load_research_summaries();
+	load_research();
+	load_industry();
+	load_sideproj();
+	load_personal();
+}
+
+var navigateTo = function(handle){
+	if($(".selected#"+handle, $("#navigation")).length == 1)
+		return;
+	$(".navigate", $("#navigation")).addClass("unselected");
+	$("#"+handle, $("#navigation")).addClass("selected").removeClass("unselected");
+
+	$(".page.selected", $(".pages")).removeClass("selected").hide();
+	$("#"+handle, $(".pages")).addClass("selected").fadeIn(300);
+}
+
+
+$(document).ready(function(){
+	
 	$(".page").hide();
 	navigateTo("about");
 	$(".navigate").click(function(v){
 		var id=v.target.id;
 		navigateTo(id);
 	})
-	populateTravel();
+	load_all();
 })
